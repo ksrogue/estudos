3b476817dce64c8d36df1e518d73e62fb128992f
David William dos Santos <ksgulosao@gmail.com>
2026-06-30 11:39:59 -0300

    algumas adições

--- DragonQuest/api.js
+++ DragonQuest/api.js
@@ -3,6 +3,12 @@ fetch("src/data/data.json")
   .then((data) => {
     const newsContainer = document.querySelector(".news");
     const gamesContainer = document.querySelector(".games-wrapper");
+    const time = new Date();
+    const formateDate = new Intl.DateTimeFormat("pt-br", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit"
+    }).format(time);
 
     data.news.forEach((news) => {
       const newCard = document.createElement("card-news");
@@ -11,18 +17,20 @@ fetch("src/data/data.json")
     //   link (a ser adicionado); 
       newCard.setAttribute("alt", news.alt);
       newCard.setAttribute("title", news.title);
+      newCard.setAttribute("time", formateDate);
+
       newsContainer.appendChild(newCard);
     });
-    data.games.forEach((games) => {
+    data.games.forEach((game) => {
         const newGame = document.createElement("games-card");
-        // imagem do jogo (a ser adicionado);
-        // alt do jogo (a ser adicionado);
+        newGame.setAttribute("src", `assets/images/games/${game.src}`);
+        newGame.setAttribute("alt", `imagem do jogo ${game.title}`);
         // link (a ser adicionado);
-        newGame.setAttribute("name", games.title);
+        newGame.setAttribute("name", game.title);
 
         gamesContainer.appendChild(newGame);
     });
   })
-  .catch((error) => console.error("Erro ao carregar Banco de Dados"));
+  .catch((error) => console.error("Erro ao carregar Banco de Dados: " + error));
 
 //    src alt link name
