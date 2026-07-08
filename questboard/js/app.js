25d8dee4b6f163b330fea75042c340bdae546189
David William dos Santos <ksgulosao@gmail.com>
2026-07-08 10:48:24 -0300

    pagina home criada

--- questboard/js/app.js
+++ questboard/js/app.js
@@ -1,1 +1,23 @@
-// task template: {"id": 1, "checked": false, "icon": "icon.png", "name": 
+// task template: {"id": 1, "checked": false, "icon": "icon.png", "name":
+
+// tema claro / escuro;
+const themeButton = document.querySelector(".theme-btn");
+let currentTheme = localStorage.getItem("theme");
+const body = document.querySelector("body");
+if (currentTheme == "dark-theme") {
+  body.classList.add("dark-theme");
+}
+themeButton.addEventListener("click", () => {
+  body.classList.toggle("dark-theme");
+  if (body.classList.contains("dark-theme")) {
+    // tema escuro;
+    localStorage.setItem("theme", "dark-theme");
+    themeButton.classList.remove("bi-moon-fill");
+    themeButton.classList.add("bi-brightness-high-fill");
+  } else {
+    // tema claro;
+    localStorage.setItem("theme", "light-theme");
+    themeButton.classList.remove("bi-brightness-high-fill");
+    themeButton.classList.add("bi-moon-fill");
+  }
+});
---
+++ questboard/js/home.js
@@ -0,0 +1,66 @@
+// const userLogged = sessionStorage.getItem("userLogged") === "true";
+const userLogged = true;
+if (!userLogged)
+  document.querySelector("body").innerHTML =
+    `<h1 style="text-align: center">O usuário precisa estar logado.</h1>`;
+//  texto de saudação;
+const greetings = document.querySelector(".greetings");
+const time = new Date();
+
+function greetingsMessage() {
+  const hour = time.getHours();
+
+  if (hour >= 12 && hour < 18) {
+    greetings.innerHTML = "BOA TARDE, DAVID!";
+  } else if (hour >= 18 && hour <= 23) {
+    greetings.innerHTML = "BOA NOITE, DAVID!";
+  } else {
+    greetings.innerHTML = "BOM DIA, DAVID!";
+  }
+}
+
+greetingsMessage();
+
+// tarefas;
+const tasks = document.querySelector(".tasks");
+const addMission = document.querySelector(".add-mission");
+const addMissionBtn = document.querySelector(".add-mission-btn");
+const addMissionContainer = document.querySelector(".add-mission-container");
+taskList = [];
+if (taskList.length == 0) {
+  addMission.style.display = "flex";
+}
+addMissionBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addMissionContainer.style.display = "flex";
+});
+
+//  salva a missão no array;
+const missionName = document.querySelector(".mission-name");
+const missionDesc = document.querySelector(".mission-desc");
+const inputStatus = document.querySelector(".input-status");
+const addBtn = document.querySelector(".add-btn");
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (missionName.value.trim() !== "" && missionDesc.value.trim() !== "") {
+    const randomXp = Math.floor(Math.random() * 9) * 5 + 10;
+    const newTask = {
+      id: Date.now(),
+      name: missionName.value,
+      desc: missionDesc.value,
+      completed: false,
+      xp: randomXp,
+    };
+    taskList.push(newTask);
+    addMissionContainer.style.display = "none";
+    missionName.value = "";
+    missionDesc.value = "";
+
+    console.log(taskList);
+    taskRender();
+  } else {
+    inputStatus.textContent = "adicione os dados";
+  }
+});
+
+// renderiza a missão na tela;
