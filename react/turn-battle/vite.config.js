d11ad649d9e538e885ffd8ccd05c8c8b5638153c
David William dos Santos <ksgulosao@gmail.com>
2026-08-21 12:19:00 -0300

    mudanças

--- react/turn-battle/vite.config.js
+++ react/turn-battle/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/estudos/react/turn-battle/",
+  base: "/estudos/react/turn-battle",
 });
